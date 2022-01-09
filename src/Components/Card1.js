bdf305ac7462103b22ca26d911eafcac10cabfb2
Federico Huneeus <fhuneeus@gmail.com>
2022-01-09 14:53:52 -0300

    Fixing images

--- src/Components/Card1.js
+++ src/Components/Card1.js
@@ -5,8 +5,6 @@ function App() {
   return (
     <div className="card1">
       <h3>Nueva colección</h3>
-      {/* <h3 className="image1-first-line">Descubre nuestra</h3>
-      <h3 className="image1-second-line">Nueva colección</h3> */}
       <img src={image1} className="body-img-1" alt="Image 1"/>
     </div>
   );
